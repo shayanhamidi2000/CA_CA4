39df3c5d8ca120cc8e353e5cc0b17a2e6d34846a
Shayunak <shayanhamidi2000@gmail.com>
2020-08-01 22:27:31 +0430

    IFandID mid-stage complete!

--- IF&ID.v
+++ IF&ID.v
@@ -1,1 +1,12 @@
-
+module IFandID(input rst,clk,write,output reg[31:0] nextInstAdr ,output reg[31:0] ir, input[31:0] adrParIn, input[31:0] instParIn);
+	always@(posedge clk , posedge rst) begin
+		if(rst) begin
+			nextInstAdr <= 32'b0;
+			ir <= 32'b0;
+		end else begin
+			nextInstAdr <= adrParIn;
+			if(write) ir <= instParIn;
+			else ir <= ir;
+		end
+	end
+endmodule
