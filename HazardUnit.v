8a839ece375b3a7c4cf2cf7d5b0518eb2b6fda59
Shayunak <shayanhamidi2000@gmail.com>
2020-08-02 22:10:19 +0430

    Control Unit Complete!

--- HazardUnit.v
+++ HazardUnit.v
@@ -5,7 +5,7 @@ module HazardUnit(input PcSrc,Jmp,memRd,input[4:0] rg1,input[4:0] rg2,input[4:0]
 		{zeroCntrl,flush} = 2'b00;
 		{PcWrite,IRWrite}= 2'b11;
 		if(ifFlush) flush = 1'b1;
-
+		
 		
 
 
--- ID.v
+++ ID.v
@@ -1,4 +1,4 @@
-module ID(input clk,rst,WrReg,branch,input[31:0] IR,input[31:0] nextInst,input[31:0] wd,input[4:0] wr,
+module ID(input clk,rst,WrReg,Brancheq,Branchneq,input[31:0] IR,input[31:0] nextInst,input[31:0] wd,input[4:0] wr,
 	output PcSrc,output[31:0] beqAdr,output[25:0] jmpAdr,output[31:0] rdReg1,output[31:0] rdReg2,
 	output[4:0] fwdReg1,output[4:0] fwdReg2,output[4:0] destReg,output[31:0] immVal,output[5:0] opcode,output[5:0] func);
 	
@@ -13,5 +13,5 @@ module ID(input clk,rst,WrReg,branch,input[31:0] IR,input[31:0] nextInst,input[3
 	assign fwdReg1 = IR[25:21];
 	assign fwdReg2 = IR[20:16];
 	assign destReg = IR[15:11];
-	assign PcSrc = (rdReg1 == rdReg2) & branch;
+	assign PcSrc = ((rdReg1 == rdReg2) & Brancheq) | (~(rdReg1 == rdReg2) & Branchneq);
 endmodule
