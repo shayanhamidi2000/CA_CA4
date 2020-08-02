d47d00611e13bbec687560e1dd5f25897b3a29b2
Shayunak <shayanhamidi2000@gmail.com>
2020-08-02 19:35:53 +0430

    fixing Ex&Mem Error!

--- HazardUnit.v
+++ HazardUnit.v
@@ -1,3 +1,16 @@
-module HazardUnit();
+module HazardUnit(input PcSrc,Jmp,memRd,input[4:0] rg1,input[4:0] rg2,input[4:0] rgDstNxt,output reg zeroCntrl,PcWrite,IRWrite,flush);
+	wire ifFlush;
+	assign ifFlush = PcSrc | Jmp;
+	always@(PcSrc,Jmp,memRd,rg1,rg2,rgDstNxt) begin
+		{zeroCntrl,flush} = 2'b00;
+		{PcWrite,IRWrite}= 2'b11;
+		if(ifFlush) flush = 1'b1;
 
+		
+
+
+
+
+
+	end
 endmodule
--- ID&EX.v
+++ ID&EX.v
@@ -1,13 +1,13 @@
 module IDandEX(input clk,rst,output reg[31:0] rg1,output reg[31:0] rg2,output reg[31:0] immVal,output reg[4:0] destReg,output reg[4:0] rdRg1, output reg[4:0] rdRg2,
-	output reg[1:0] AluOp,output reg AluSrc,RegDst,MemWr,MemRd,DataSrc,WrReg,
+	output reg[2:0] AluOperation,output reg AluSrc,RegDst,MemWr,MemRd,DataSrc,WrReg,
 	input[31:0] Inrg1,input[31:0] Inrg2,input[31:0] InimmVal,input[4:0] IndestReg,input[4:0] InrdRg1, input[4:0] InrdRg2,
-	input[1:0] InAluOp,input InAluSrc,InRegDst,InMemWr,InMemRd,InDataSrc,InWrReg);
+	input[2:0] InAluOperation,input InAluSrc,InRegDst,InMemWr,InMemRd,InDataSrc,InWrReg);
 	
 	always@(posedge rst,posedge clk) begin
-		if(rst)	{rg1,rg2,immVal,destReg,rdRg1,rdRg2,AluOp,AluSrc,RegDst,MemWr,MemRd,DataSrc,WrReg} <= 119'b0; 
+		if(rst)	{rg1,rg2,immVal,destReg,rdRg1,rdRg2,AluOperation,AluSrc,RegDst,MemWr,MemRd,DataSrc,WrReg} <= 120'b0; 
 		else begin
 			rg1 <= Inrg1; rg2 <= Inrg2; immVal <= InimmVal; destReg <= IndestReg; rdRg1 <= InrdRg1; rdRg2 <= InrdRg2;
-			AluOp <= InAluOp; AluSrc <= InAluSrc; RegDst <= InRegDst; MemWr <= InMemWr; MemRd <= InMemRd; DataSrc <= InDataSrc;
+			AluOperation <= InAluOperation; AluSrc <= InAluSrc; RegDst <= InRegDst; MemWr <= InMemWr; MemRd <= InMemRd; DataSrc <= InDataSrc;
 			WrReg <= InWrReg;
 		end
 	end
