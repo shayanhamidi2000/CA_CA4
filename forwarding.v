458e5b47d0d3c708e72c83fb6f12a26e734f8af5
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2020-08-04 20:27:46 +0430

    ports size

--- forwarding.v
+++ forwarding.v
@@ -1,4 +1,4 @@
-module ForwardingUnit(input EXMEMregWr, EXMEMrd, input IDEXrs, IDEXrt, input MEMWBregWr, MEMWBrd,
+module ForwardingUnit(input EXMEMregWr, input[4:0] EXMEMrd, IDEXrs, IDEXrt, input MEMWBregWr, input[4:0] MEMWBrd,
        output[1:0] src1, src2);
        
        assign src1 = (EXMEMregWr == 1 && EXMEMrd == IDEXrs && EXMEMrd != 5'b0) ? 2'b10 : (MEMWBregWr == 1 && MEMWBrd == IDEXrs && MEMWBrd != 5'b0) ? 2'b01 : 2'b0;
