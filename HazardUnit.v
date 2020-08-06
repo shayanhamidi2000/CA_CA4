c62266e2ed01f35ef156f908ba303c16d2f58912
Mahdi <49525652+SMahdiHosseini@users.noreply.github.com>
2020-08-06 15:49:18 +0430

    change Symblos

--- HazardUnit.v
+++ HazardUnit.v
@@ -2,17 +2,17 @@ module HazardUnit(input PcSrc,Jmp,branch,memRd,input[4:0] rg1,input[4:0] rg2,inp
 	wire ifFlush;
 	assign ifFlush = PcSrc | Jmp;
 	always@(PcSrc,Jmp,memRd,rg1,rg2,rgDstNxt) begin
-		{zeroCntrl,flush} = 2'b00;
-		{PcWrite,IRWrite}= 2'b11;
+		{zeroCntrl,flush} <= 2'b00;
+		{PcWrite,IRWrite} <= 2'b11;
 		if(branch & (rgDstNxt != 5'b0) & ((rgDstNxt == rg1) | (rgDstNxt == rg2)) ) begin
-			zeroCntrl = 1'b1;
-			PcWrite = 1'b0;
-			IRWrite = 1'b0;
+			zeroCntrl <= 1'b1;
+			PcWrite <= 1'b0;
+			IRWrite <= 1'b0;
 		end else if(ifFlush) flush = 1'b1;
 		else if(memRd & (rgDstNxt != 5'b0) & ((rgDstNxt == rg1) | (rgDstNxt == rg2)) ) begin
-			zeroCntrl = 1'b1;
-			PcWrite = 1'b0;
-			IRWrite = 1'b0;	
+			zeroCntrl <= 1'b1;
+			PcWrite <= 1'b0;
+			IRWrite <= 1'b0;	
 		end
 	end
 endmodule
